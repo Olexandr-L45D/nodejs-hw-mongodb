cf953c93db6b58d159075589e19f49b239dfd516
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-11-10 12:36:33 +0200

    add function login, logout, refresh

---
+++ src/validation/auth.js
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+export const registerUserSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required().messages({
+        'string.base': 'Username should be a string',
+        'string.min': 'Username should have at least {#limit} characters',
+    }),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+});
+
+export const loginUserSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+});
