b9574850a899980d0b0775df79ec7654687bc992
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-11-10 10:08:39 +0200

    add model user

---
+++ src/db/models/session.js
@@ -0,0 +1,26 @@
+// src/db/models/session.js
+
+import { model, Schema } from 'mongoose';
+
+const Session = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+    // Id юзера, якому належить сесія
+    accessToken: { type: String, required: true },
+    // accessToken - короткоживучий(в нашому випадку 15 хвилин) токен, який браузер буде нам додавати в хедери запитів (хедер Authorization)
+    refreshToken: { type: String, required: true },
+    // Refresh токену - більш довгоживучому (в нашому випадку 1 день, але може бути і більше) токену, який можна буде обміняти на окремому ендпоінті на нову пару access + resfresh токенів. Зберігається в cookies
+    accessTokenValidUntil: { type: Date, required: true },
+    // accessTokenValidUntil - Терміну життя access токену
+    refreshTokenValidUntil: { type: Date, required: true },
+
+},
+    { timestamps: true, versionKey: false },
+);
+export const SessionCollection = model('sessions', Session);
+// Створіть модель Session з такими полями:
+// userId - string, required
+// accessToken - string, required
+// refreshToken - string, required
+// accessTokenValidUntil - Date, required
+// refreshTokenValidUntil - Date, required
+// export const UsersCollection = model('users', usersSchema);
---
+++ src/db/models/user.js
@@ -0,0 +1,37 @@
+// new model to BD for auth
+import { model, Schema } from 'mongoose';
+
+const User = new Schema(
+    {
+        name: { type: String, requirerd: true },
+        email: { type: String, requirerd: true, unique: true },
+        password: { type: String, requirerd: true },
+        createdAt: { type: Date },
+        updatedAt: { type: Date },
+
+    },
+    { timestamps: true, varsionKey: false },
+);
+User.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+};
+
+export const UsersCollection = model('users', User);
+
+
+
+
+
+
+
+
+
+
+// import { ROLES } from '../constants/index.js';
+// role: {
+//     type: String,
+//             enum: [ROLES.TEACHER, ROLES.PARENT],
+//             default: ROLES.PARENT,
+//         },
