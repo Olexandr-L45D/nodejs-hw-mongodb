b2ee170475193c3693bf62fcac89927950607827
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-11-19 12:11:33 +0200

    templates

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ import path from 'node:path';
 export const TEMPLATES_DIR = path.resolve("src", "templates");
 
 // export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
-export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'templates');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 // Ці налаштування дозволяють зберігати завантажені файли у визначеній директорії з унікальними іменами, що забезпечить організоване та безпечне управління файлами на сервері.
 export const SORT_ORDER = {
