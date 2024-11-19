ecceb4aa7058c74e272c6cd3bd7da4fc1f48d09c
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-11-19 12:21:26 +0200

    upd

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -24,3 +24,23 @@ export const resetPasswordSchema = Joi.object({
     password: Joi.string().required(),
     token: Joi.string().required(),
 });
+
+
+
+//  значення які вставив в файл оточення (env) для перевірки так як мої були не авторизовані
+// SMTP_HOST = smtp - relay.brevo.com
+// SMTP_PORT = 587
+// SMTP_USER = 765fb3001@smtp-brevo.com
+// SMTP_PASSWORD = xsmtpsib-f533f9d1eab7ec239c37eaa378366307ac9ba7c13c08c93f0a608c40d176d64d-sKxkNGfz7BHpvEbA
+// SMTP_FROM = vladislavapelhants@gmail.com
+
+
+// "name": "Vladislav",
+//     "email": "vladislavapelhants@gmail.com",
+//         "password": "123Vlad"
+
+// SMTP_HOST = smtp - relay.brevo.com
+// SMTP_PORT = 587
+// SMTP_USER = 801450002@smtp-brevo.com
+// SMTP_PASSWORD = xsmtpsib-2b237ed81d4253bd91cf66cb162760c211eefba83bb6f53d253def9a86bd9a28-PTgWr5tHJvAfVRSc
+// SMTP_FROM = litvinenko1978aleks@gmail.com
